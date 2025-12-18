83a121f4ae8bd52cb13dd17d93df4be3699c5b23
Sumit_Patil <sumitpatil2552@gmail.com>
2025-12-18 14:55:09 +0530

    Implement insertion sort in sorting.java
    Added insertion sort method to the sorting class.

--- sorting.java
+++ sorting.java
@@ -18,7 +18,7 @@ public class sorting {
         }
     }
 
-
+    //-------------SelectionSort----------------
     public static void SelectionSort(int nums[]){
         for (int i=0;i<nums.length-1;i++){
             int min=i;
@@ -32,6 +32,22 @@ public class sorting {
             nums[i]=temp;
         }
     }
+    //----------------insertionSort-----------------
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i]; // Current element to be inserted
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        print(arr);
+    }
+
 
     public static void print(int nums[]){
         for(int i= 0;i<nums.length;i++){
