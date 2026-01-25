871f7acc003ed03090fa3811d1778b78f2099bf3
Sumit_Patil <sumitpatil2552@gmail.com>
2026-01-25 23:07:29 +0530

    Refactor array class and add setZeroes method

--- array.java
+++ array.java
@@ -46,6 +46,33 @@ public class array {
        
         return ms;
     }
+// 73. Set Matrix Zeroes
+
+    public static void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
 
 
     // tap water imp
