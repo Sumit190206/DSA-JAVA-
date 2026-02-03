25c648723c27cbb16369f04d93c96a64d0102e19
Sumit_Patil <sumitpatil2552@gmail.com>
2026-02-03 23:05:01 +0530

    Refactor sorting class by removing unused methods
    Removed commented-out sorting methods and cleaned up code.

--- sorting.java
+++ sorting.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class sorting {
     public static void bubbleSort(int nums[]){
         int n = nums.length;
@@ -64,70 +64,4 @@ public class sorting {
     }
     
 }
-=======
-public class sorting {
-    public static void bubbleSort(int nums[]){
-        int n = nums.length;
-        for(int i=0;i<n-1;i++){
-            int swaped=0;
-            for (int j=0;j<n-i-1;j++){
-                if(nums[j]>nums[j+1]){
-                    int temp= nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-                    swaped++;
-                }
-            }
-            if(swaped==0){
-                break;
-            }
-            System.out.println(swaped+ " ");
-        }
-    }
 
-    //-------------SelectionSort----------------
-    public static void SelectionSort(int nums[]){
-        for (int i=0;i<nums.length-1;i++){
-            int min=i;
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[min]>nums[j]){
-                    min=j;
-                }
-            }
-            int temp = nums[min];
-            nums[min]=nums[i];
-            nums[i]=temp;
-        }
-    }
-    //----------------insertionSort-----------------
-    public static void insertionSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 1; i < n; i++) {
-            int key = arr[i]; // Current element to be inserted
-            int j = i - 1;
-
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = key;
-        }
-        print(arr);
-    }
-
-
-    public static void print(int nums[]){
-        for(int i= 0;i<nums.length;i++){
-            System.out.print(nums[i]+ " ");
-        }
-        System.out.println("");
-    }
-    public static void main(String[] args) {
-        int nums[]={3,6,7,2,1,3};
-        SelectionSort(nums);
-        // bubbleSort(nums);
-        print(nums);
-    }
-    
-}
->>>>>>> a854106cc8f5235b2577856e2454c1eb967dbc93
