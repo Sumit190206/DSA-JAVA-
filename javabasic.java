e3dc7651837829db189d482ab19aa525d30bce1b
Sumit_Patil <sumitpatil2552@gmail.com>
2026-02-03 23:02:46 +0530

    Refactor javabasic.java to remove duplicate code
    Removed duplicate code and cleaned up the class structure.

--- javabasic.java
+++ javabasic.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.util.*;
 public class javabasic {
     public static void Bin(int n){
@@ -18,25 +18,5 @@ public class javabasic {
         Bin(111);
     sc.close();
     }
-=======
-import java.util.*;
-public class javabasic {
-    public static void Bin(int n){
-        int m=n;
-        int pow=0;
-        int dec=0;
-        while(n>0){
-            int last = n%10;
-            dec=dec+(last*(int)Math.pow(2, pow));
-            pow++;
-            n=n/10;
-        }
-        System.out.println(m + " Is converted in to ::" + dec);
-    }
-       public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-        Bin(111);
-    sc.close();
-    }
->>>>>>> a854106cc8f5235b2577856e2454c1eb967dbc93
+
 }
