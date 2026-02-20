ba4c05e66ee423b6bbaaca8fb4a0f133b662e569
Sumit Patil <sumitpatil2552@gmail.com>
2026-02-20 18:36:02 +0530

    Added a function to add element at bottom of stack

--- StackA.java
+++ StackA.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 public class StackA {
     static class Node{
         int data;
@@ -10,7 +10,8 @@ public class StackA {
         }
     }
 
-    static class Stack {
+    // static class Stack {
+// --------------------------Stack opration using arraylist----------------------------
     //     static ArrayList<Integer>list= new ArrayList<>();
 
     //     public static Boolean isEmpty(){
@@ -28,41 +29,54 @@ public class StackA {
     //         int top = list.get(list.size()-1);
     //         return top;
     //     }
-        static Node head = null;
-        public static boolean isEmpty(){
-            return head == null;
-        }
-        public static void push(int data){
-            Node temp = new Node(data);
-            if(isEmpty()){
-               head = temp;
-               return;
-            } 
-            temp.next =head;
-            head=temp;
-        }
-        public static int pop(){
-            if(isEmpty()){
-                return-1;
-            }
-           int top = head.data;
-           head = head.next;
-           return top;
-        }
-        public static int peek(){
-            if(isEmpty()){
-                return -1 ;
-            }
+// ---------------------------Stack opration using Linked list----------------------------
+    //     static Node head = null;
+    //     public static boolean isEmpty(){
+    //         return head == null;
+    //     }
+    //     public static void push(int data){
+    //         Node temp = new Node(data);
+    //         if(isEmpty()){
+    //            head = temp;
+    //            return;
+    //         } 
+    //         temp.next =head;
+    //         head=temp;
+    //     }
+    //     public static int pop(){
+    //         if(isEmpty()){
+    //             return-1;
+    //         }
+    //        int top = head.data;
+    //        head = head.next;
+    //        return top;
+    //     }
+    //     public static int peek(){
+    //         if(isEmpty()){
+    //             return -1 ;
+    //         }
             
-            return head.data;
+    //         return head.data;
+    //     }
+    // }
+    public static void pushAtBottom(Stack<Integer>s,int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
         }
+
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
     }
     public static void main(String[] args) {
-        Stack s = new Stack();
+    //    Stack s = new Stack();
         // ArrayList<Integer> list = new ArrayList<>();
+         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(4);
         s.push(3);
+        pushAtBottom(s,5);
         while(!s.isEmpty()){
           System.out.println(s.peek());
           s.pop();
