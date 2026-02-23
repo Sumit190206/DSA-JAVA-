6d779b17f929913b5fbb3356cb766360c8762bd7
Sumit Patil <sumitpatil2552@gmail.com>
2026-02-23 23:23:46 +0530

    Reverse a stack

--- StackA.java
+++ StackA.java
@@ -91,11 +91,7 @@ public class StackA {
         reverseStack(s);
         pushAtBottom(s, top);
     }
-    public static void printStack(Stack<Integer> s){
-        while(!s.isEmpty()){
-            System.out.println(s.pop());
-        }
-    }
+   
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
