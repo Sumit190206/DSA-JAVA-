d0b2e6b4323e831a4aebc899fc216c60e3023ad7
Sumit Patil <sumitpatil2552@gmail.com>
2026-03-05 23:53:56 +0530

    Solution of leetcode problem  No 24 is added

--- Linklist.java
+++ Linklist.java
@@ -331,6 +331,28 @@ public class Linklist {
             }
             return prev;
         }
+        
+    public Node swapPairs(Node head) {
+        
+        Node dummy = new Node(0);
+        dummy.next = head;
+        
+        Node prev = dummy;
+        
+        while(prev.next != null && prev.next.next != null){
+            
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+            
+            prev = first;
+        }
+        
+        return dummy.next;
+    }
+
     public static void main(String[] args) {
     //     Linklist ll = new Linklist();
     //    ll.addFirst(2);
