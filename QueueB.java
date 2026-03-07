2ddcc5f04490fc4d369a85c43f443bda47437cb1
Sumit Patil <sumitpatil2552@gmail.com>
2026-03-07 22:56:32 +0530

    Queue using linked list

--- QueueB.java
+++ QueueB.java
@@ -37,9 +37,105 @@ public class QueueB {
             return arr[0];
         }
     }
+    // =============circular queue======================
+    static class QueueCi {
+        static int  arr[];
+        static int size;
+        static int front;
+        static int rear;
+        QueueCi(int n){
+            arr=new int[n];
+            size = n;
+            rear =-1;
+            front =-1;
+        }
+        public static boolean isEmpty(){
+            return rear == -1 && front == -1;
+        }
+        public static boolean isfull(){
+            return (rear+1)%size == front;
+        }
+        public static void add(int data){
+            if(isfull()){
+                System.out.println("array is full");
+                return;
+            }
+            if(front ==-1){
+                front = 0;
+            }
+            rear = (rear+1)%size;
+            arr[rear]=data;
+        }
+        public static int remove(){
+            if(isEmpty()){
+                return -1;
+            }
+            int result = arr[front];
+            if(front == rear){
+                front =-1;
+                rear = -1;
+            }
+            else{
+                front =(front+1)%size;
+            }
+            return result;
+        }
+        public static int peek(){
+             if(isEmpty()){
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+    static class  Node{
+       int data;
+       Node next;
+
+       Node(int data){
+        this.data=data;
+        this.next = null;
+       }
+    }
+    static class QueueLL{
+       static Node head = null;
+       static Node tail = null;
+       
+       public static boolean isEmpty(){
+        return head == null && tail ==null;
+       }
+       public static void add(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail =newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail = newNode;
+       }
+       public static int remove(){
+        if(isEmpty()){
+            return-1;
+        }
+        Node temp = head;
+        if(head == tail){
+            head=null;
+            tail=null;
+        }
+        else{
+            head=head.next;
+        }
+        return temp.data;
+       }
+       public static int peek(){
+        if(isEmpty()){
+            return -1;
+        }
+        return head.data;
+       }
+    }
 
     public static void main(String[] args) {
-        Queue q = new Queue(5);
+        QueueLL q = new QueueLL();
         q.add(1);
         q.add(2);
         q.add(3);
@@ -48,5 +144,6 @@ public class QueueB {
             q.remove();
         }
     }
+
     
 }
