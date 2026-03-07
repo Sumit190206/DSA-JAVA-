5a7edfd34bbf0fecea9bedcad08b14061694b06b
Sumit Patil <sumitpatil2552@gmail.com>
2026-03-07 23:19:58 +0530

    Queue using linked list

--- QueueB.java
+++ QueueB.java
@@ -135,13 +135,7 @@ public class QueueB {
     }
 
     public static void main(String[] args) {
-        QueueLL q = new QueueLL();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        while (!q.isEmpty()) {
-            System.out.println(q.peek());
-            q.remove();
+       ve();
         }
     }
 
