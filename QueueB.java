304712ce8696d0324fe8304274414af19b55add3
Sumit Patil <sumitpatil2552@gmail.com>
2026-03-11 21:38:47 +0530

    Queue using two stack

--- QueueB.java
+++ QueueB.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class QueueB {
     static class Queue {
         static int  arr[];
@@ -35,6 +36,36 @@ public class QueueB {
                 return -1;
             }
             return arr[0];
+        }
+        // ============  QUEUE USING TWO STACK  ===================
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+        public static boolean isempty(){
+            return s1.isEmpty();
+        }
+
+        public static void addd(int data){
+           while(!s1.isEmpty()){
+            s2.push(s1.pop());
+           }
+           s1.push(data);
+           while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+           }
+        }
+        public static int remoove(){
+            if(isempty()){
+                System.out.println("QUEUE IS EMPTY");
+                return-1;
+            }
+            return s1.pop();
+        }
+        public static int peekk(){
+            if(isempty()){
+                System.out.println("QUEUE IS EMPTY");
+                return-1;
+            }
+            return s1.peek();
         }
     }
     // =============circular queue======================
@@ -135,15 +166,16 @@ public class QueueB {
     }
 
     public static void main(String[] args) {
-        QueueLL q = new QueueLL();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        while (!q.isEmpty()) {
-            System.out.println(q.peek());
-            q.remove();
+        Queue q = new Queue(3);
+        q.addd(1);
+        q.addd(2);
+        q.addd(3);
+        while(!q.isempty()){
+            System.out.println(q.peekk());
+            q.remoove();
+        }
         }
     }
 
     
-}
+
