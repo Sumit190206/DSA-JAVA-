fe3dda5561f89b76e7b6bc109c66c834d77f8da6
Sumit Patil <sumitpatil2552@gmail.com>
2026-03-12 23:03:12 +0530

    Queue & Stack is implemented using Deque

--- Classroom.java
+++ Classroom.java
@@ -45,15 +45,50 @@ public class Classroom {
 
             
         }
+        static class queue {
+        Deque<Integer>d= new LinkedList<>();
+
+        public boolean isEmpty(){
+            return d.isEmpty();
+        }
+        public  void add (int data){
+            d.addLast(data);
+        }
+        public  int remove(){
+            if(d.isEmpty()){
+                return -1;
+            }
+            return d.removeFirst();
+        }
+        public int peek(){
+            if(d.isEmpty()){
+                return -1;
+            }
+            return d.getFirst()  ;
+        }
+
+            
+        }
     public static void main(String[] args) {
        stack s = new stack();
        s.push(1);
        s.push(2);
        s.push(3);
        s.push(4);
-
+       System.out.println("Stack ::" + "peek = "+ s.peek());
        while(!s.isEmpty()){
         System.out.print(s.pop()+" ");
        }
+       System.out.println();
+       queue q = new queue();
+       q.add(1);
+       q.add(2);
+       q.add(3);
+       q.add(4);
+       System.out.println("Queue" + "peek = "+ q.peek());
+       while (!q.isEmpty()) {
+        System.out.print(q.remove()+" ");
+       }
+       System.out.println();
     }
 }
