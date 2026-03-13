b818a941f31aef7aa4fefb1f9ea9fc17137e2c94
Sumit Patil <sumitpatil2552@gmail.com>
2026-03-13 23:19:02 +0530

    Queue & Stack is implemented using Deque

--- Classroom.java
+++ Classroom.java
@@ -3,6 +3,16 @@ public class Classroom {
     // ============== InterLeaves ====================
     public static void interLeaves(Queue<Integer>q){
       
+          int  size = q.size();
+        Queue<Integer>q2 = new LinkedList<>();
+        for(int i = 0; i<size/2;i++){
+            q2.add(q.remove());
+        }
+        while (!q2.isEmpty()) {
+            q.add(q2.remove());
+            q.add(q.remove());
+        }
+
     }
     // =========== Reverse a Queue =============
     public static void queueReversal(Queue<Integer>q){
