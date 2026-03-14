8e49930f83c7b75198c089c7700493d6a9e5616e
Sumit Patil <sumitpatil2552@gmail.com>
2026-03-14 22:58:16 +0530

    Leetcode Q.15 Solution is added

--- Arraylist1.java
+++ Arraylist1.java
@@ -179,4 +179,50 @@ public class Arraylist1 {
 
         return increasing || decreasing;
     }
+// leetcode 16 ;
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    List<Integer> l1 = new ArrayList<>();
+                    l1.add(nums[i]);
+                    l1.add(nums[left]);
+                    l1.add(nums[right]);
+                    result.add(l1);
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+
+                } 
+                else if (sum < 0) {
+                    left++;
+                } 
+                else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
 }
