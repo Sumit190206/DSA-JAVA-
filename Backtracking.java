81ad3f7346d55a64dde2d65af7cc745c166ae1c4
Sumit Patil <sumitpatil2552@gmail.com>
2026-04-07 23:50:40 +0530

    new file

--- Backtracking.java
+++ Backtracking.java
@@ -9,7 +9,13 @@ public class Backtracking {
         
 
         
-           
+           for(int j=0;j<arr.length;j++){
+            if(isSafe(arr, row, j)){
+                 arr[row][j]='Q';
+                 nQueens(arr, row+1);
+                 arr[row][j]='x';
+  
+            }
         }
     }
     public static boolean isSafe(char arr[][],int row,int col){
